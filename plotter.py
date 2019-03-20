a13aa6841401e76541fffe132b883feffd0e90bc
dormrod <david.ormrodmorley@balliol.ox.ac.uk>
2019-03-20 20:38:28 +0000

    Set axes properties (labels,ticks etc.)

--- plotter.py
+++ plotter.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
+import matplotlib.ticker as ticker
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
@@ -9,7 +11,7 @@ class Plot:
 
     ##### Functions to control plot settings #####
 
-    def __int__(self):
+    def __init__(self):
         """Set default parameters"""
 
         self._num_datasets = 0 # Total number of added data sets
@@ -19,6 +21,7 @@ class Plot:
         self.set_plot_size() # Initialise plot size to 4x4cm
         self.set_text_size() # Initialise text size to 10pt
         self.set_dimensions() # 2D/3D plot
+        self.set_axes_properties() # Default axes labels
 
 
     def set_plot_size(self, width = 4, height = 4):
@@ -47,6 +50,23 @@ class Plot:
             self._dimensions = dim
 
 
+    def set_axes_properties(self, **kwargs):
+        """Sets axes labels, limits and ticks etc."""
+
+        self._axis_xlabel = kwargs.get("xlabel", "X")
+        self._axis_ylabel = kwargs.get("ylabel", "Y")
+        self._axis_zlabel = kwargs.get("zlabel", "Z")
+        self._axis_xlim = kwargs.get("xlim", None)
+        self._axis_ylim = kwargs.get("ylim", None)
+        self._axis_zlim = kwargs.get("zlim", None)
+        self._axis_xticks = kwargs.get("xticks", None)
+        self._axis_yticks = kwargs.get("yticks", None)
+        self._axis_zticks = kwargs.get("zticks", None)
+        self._axis_xlog = kwargs.get("xlog", False)
+        self._axis_ylog = kwargs.get("ylog", False)
+        self._axis_zlog = kwargs.get("zlog", False)
+
+
     ##### Functions to add data sets #####
 
     def add_dataset(self,data,**kwargs):
@@ -65,11 +85,15 @@ class Plot:
     def initialise_plot(self):
         """Initialise axis and figure"""
 
-        # Initialise plot if not already called
+        # Initialise plot if not already called as 2D or 3D plot
         if self._initialised:
             pass
         else:
-            self._fig, self._ax = plt.subplots()
+            if self._dimensions == 2:
+                self._fig, self._ax = plt.subplots()
+            elif self._dimensions == 3:
+                self._fig = plt.figure()
+                self._ax = Axes3D(self._fig)
             self._initialised = True
 
 
@@ -80,6 +104,33 @@ class Plot:
         if self._finalised:
             pass
         else:
+            # Axes properties
+            self._ax.set_xlabel(self._axis_xlabel)
+            self._ax.set_ylabel(self._axis_ylabel)
+            if self._axis_xlim is not None: self._ax.set_xlim(self._axis_xlim)
+            if self._axis_ylim is not None: self._ax.set_xlim(self._axis_xlim)
+            if self._axis_xticks is not None:
+                majorLocator = ticker.MultipleLocator(self._axis_xticks[0])
+                minorLocator = ticker.MultipleLocator(self._axis_xticks[1])
+                self._ax.xaxis.set_major_locator(majorLocator)
+                self._ax.xaxis.set_minor_locator(minorLocator)
+            if self._axis_yticks is not None:
+                majorLocator = ticker.MultipleLocator(self._axis_yticks[0])
+                minorLocator = ticker.MultipleLocator(self._axis_yticks[1])
+                self._ax.yaxis.set_major_locator(majorLocator)
+                self._ax.yaxis.set_minor_locator(minorLocator)
+            if self._axis_xlog: self._ax.set_xscale('log')
+            if self._axis_ylog: self._ax.set_yscale('log')
+            if self._dimensions == 3:
+                # 3D additions
+                self._ax.set_zlabel(self._axis_zlabel)
+                if self._axis_zlim is not None: self._ax.set_zlim(self._axis_zlim)
+                if self._axis_zticks is not None:
+                    majorLocator = ticker.MultipleLocator(self._axis_zticks[0])
+                    minorLocator = ticker.MultipleLocator(self._axis_zticks[1])
+                    self._ax.zaxis.set_major_locator(majorLocator)
+                    self._ax.zaxis.set_minor_locator(minorLocator)
+
             self._finalised = True
 
 
@@ -103,3 +154,6 @@ class Plot:
 
 if __name__ == "__main__":
     plot = Plot()
+    xy = np.random.rand(100)
+    xy = xy.reshape(50,2)
+    plot.add_dataset(xy)
