9fe0693ce304d4fad6aa841513c6a96837a47232
dormrod <david.ormrodmorley@balliol.ox.ac.uk>
2019-03-21 11:31:11 +0000

    Add error plots

--- tester.py
+++ tester.py
@@ -3,11 +3,19 @@ import numpy as np
 
 random_xy1 = np.random.rand(100,2)
 random_xy2 = np.random.rand(100,2)
-random_ds1=DataSet(random_xy1)
-random_ds2=DataSet(random_xy2)
+sine = np.zeros((100,2))
+sine[:,0] = np.linspace(0,10,100)
+sine[:,1] = np.sin(sine[:,0])
+cos = np.zeros_like(sine)
+cos[:,0] = sine[:,0]
+cos[:,1] = np.cos(cos[:,0])
+random_error = np.random.rand(100)
+
+sine_ds=DataSet(sine,error_y=random_error,error_style='shade')
+cos_ds=DataSet(cos)
 
 plot=Plot()
-plot.add_dataset(random_ds1)
-plot.add_dataset(random_ds2)
+plot.add_dataset(sine_ds)
+plot.add_dataset(cos_ds)
 plot.plot()
 plot.display()
